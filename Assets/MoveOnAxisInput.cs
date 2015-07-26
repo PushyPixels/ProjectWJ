12b1f68109eff277cbb08fb0292dd0a307f447b5
Maximilian Maroe <drakfyre@gmail.com>
2015-07-25 19:20:13 -0700

    Basic arena setup and basic movement

---
+++ Assets/MoveOnAxisInput.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOnAxisInput : MonoBehaviour
+{
+	public string horizontalAxis = "Horizontal";
+	public string verticalAxis = "Vertical";
+	public float speed = 5.0f;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 moveDirection = Vector3.right*Input.GetAxis(horizontalAxis) + Vector3.forward*Input.GetAxis(verticalAxis);
+		if(moveDirection.sqrMagnitude > 0.0f)
+		{
+			transform.rotation = Quaternion.LookRotation(moveDirection,Vector3.up);
+		}
+
+		transform.position += moveDirection*speed*Time.deltaTime;
+	}
+}
